ed4f2655cf4855c00e023b493f9f2de0cdcc8e74
Leonardo Lavagna <llavagna@proton.me>
2025-02-10 13:04:31 +0100

    Add files via upload

---
+++ utilities/born_machine.py
@@ -0,0 +1,272 @@
+#------------------------------------------------------------------------------
+# QUANTUM BORN MACHINE FOR QUANTUM GENERATIVE MODELING VIA STRAIGHTFORWARD STATE PREPARATION
+# @ NesyaLab: github.com/NesyaLab
+#
+# References
+#    [1] https://github.com/GiggleLiu/QuantumCircuitBornMachine/
+#    [2] https://arxiv.org/abs/1804.04168
+#------------------------------------------------------------------------------
+
+
+import numpy as np
+import scipy.sparse as sps
+
+
+# GLOBAL VARIABLES (PAULI MATRIXES)
+I2 = sps.eye(2).tocsr()
+sx = sps.csr_matrix([[0,1],[1,0.]])
+sy = sps.csr_matrix([[0,-1j],[1j,0.]])
+sz = sps.csr_matrix([[1,0],[0,-1.]])
+
+# GLOBAL VARIABLES (PROJECTORS)
+p0 = (sz + I2) / 2  
+p1 = (-sz + I2) / 2
+
+
+def rot_matrix(si, theta):
+    """
+    Single qubit rotation
+    """
+    return np.cos(theta/2.)*I2 - 1j*np.sin(theta/2.)*si
+
+
+def rot(t1, t2, t3):
+    '''
+    a general rotation gate rz(t3)rx(r2)rz(t1).
+
+    Args:
+        t1, t2, t3 (float): three angles.
+
+    Returns:
+        2x2 csr_matrix: rotation matrix.
+    '''
+    return rot_matrix(sz, t3).dot(rot_matrix(sx, t2)).dot(rot_matrix(sz, t1))
+
+
+def CNOT(ibit, jbit, n):
+    '''
+    CNOT gate
+    
+    Args:
+        ibit (int): control bit.
+        jbit (int): controled bit.
+        n (int): total number of qubits.
+        
+    Returns:
+        4x4 csr_matrix: CNOT matrix.
+    '''
+    res = compiler([p0, I2], [ibit, jbit], n)
+    res = res + compiler([p1, sx], [ibit, jbit], n)
+    return res
+
+
+def compiler(ops, locs, n):
+    '''
+    Compile operators into specific Hilbert space.
+
+    Args:
+        ops (list): list of single bit operators.
+        locs (list): list of positions.
+        n (int): total number of bits.
+
+    Returns:
+        csr_matrix: resulting matrix.
+
+    Remark: the last bit is the high end bit
+    '''
+    if np.ndim(locs) == 0:
+        locs = [locs]
+    if not isinstance(ops, (list, tuple)):
+        ops = [ops]
+    locs = np.asarray(locs)
+    locs = n - locs
+    order = np.argsort(locs)
+    locs = np.concatenate([[0], locs[order], [n + 1]])
+    return wrap_identity([ops[i] for i in order], np.diff(locs) - 1)
+
+
+def wrap_identity(data_list, num_bit_list):
+    if len(num_bit_list) != len(data_list) + 1:
+        raise Exception()
+
+    res = sps.eye(2**num_bit_list[0])
+    for data, nbit in zip(data_list, num_bit_list[1:]):
+        res = sps.kron(res, data)
+        res = sps.kron(res, sps.eye(2**nbit, dtype='complex128'))
+    return res
+
+
+def initial_wf(num_bit):
+    '''initial wave function |00...0>.'''
+    wf = np.zeros(2**num_bit, dtype='complex128')
+    wf[0] = 1.
+    return wf
+
+
+def get_nn_pairs(num_bit):
+    '''get nearest neighbor pairs (CNOTs are applied to these pairs).'''
+    res = []
+    for inth in range(2):
+        for i in range(inth, num_bit, 2):
+            res = res + [(i, i_ % num_bit) for i_ in range(i + 1, i + 2)]
+    return res
+
+def get_circuit(num_bit, depth, pairs):
+    '''Get the circuit used for demo'''
+    blocks = []
+    # build circuit
+    for idepth in range(depth+1):
+        blocks.append(ArbitraryRotation(num_bit))
+        if idepth!=depth:
+            blocks.append(CNOTEntangler(num_bit, pairs))
+
+    # set leading and trailing Rz to disabled
+    blocks[0].mask[::3] = False
+    blocks[-1].mask[2::3] = False
+    return BlockQueue(blocks)
+
+
+
+
+
+
+
+
+class ArbitraryRotation(object):
+    '''Arbitrary rotation gate'''
+    def __init__(self, num_bit):
+        self.num_bit = num_bit
+        # mask is used to filter out some irrelevant (marked False) parameters
+        self.mask = np.array([True] * (3*num_bit), dtype='bool')
+
+    
+    @property
+    def num_param(self):
+        return self.mask.sum()
+
+    
+    def tocsr(self, theta_list):
+        '''transform this block to a sequence of csr_matrices.'''
+        theta_list_ = np.zeros(3*self.num_bit)
+        theta_list_[self.mask] = theta_list
+        rots = [rot(*ths) for ths in theta_list_.reshape([self.num_bit,3])]
+        res = [compiler([r], [i], self.num_bit) for i,r in enumerate(rots)]
+        return res
+    
+    
+class CNOTEntangler(object):
+    '''
+    CNOT Entangler Layer.
+    
+    Args:
+        pairs (list): a list of tuples to represent connections.
+    '''
+    def __init__(self, num_bit, pairs):
+        self.num_bit = num_bit
+        self.pairs = pairs
+
+    
+    @property
+    def num_param(self):
+        return 0
+
+    
+    def tocsr(self, theta_list):
+        '''transform this block to a sequence of csr_matrices.'''
+        i, j = self.pairs[0]
+        res = CNOT(i, j, self.num_bit)
+        for i, j in self.pairs[1:]:
+            res = CNOT(i,j,self.num_bit).dot(res)
+        res.eliminate_zeros()
+        return [res]
+    
+    
+class BlockQueue(list):
+    '''
+    Block Queue that keep track of theta_list changing history, for fast update.
+    
+    Attributes:
+        theta_last (1darray): the cached circuit parameters.
+        memo (list): cached sparse matrices for layers.
+    '''
+    def __init__(self, *args):
+        list.__init__(self, *args)
+        self.theta_last = None
+        self.memo = None
+
+    def __call__(self, qureg, theta_list):
+        '''
+        Apply operations on vector basis: qureg. This operation changes vector inplace.
+        
+        Args:
+            qureg (1darray): vector basis.
+            theta_list (1darray): circuit parameters:
+        '''
+        # cache? if theta_list change <= 1 parameters, then don't touch memory.
+        remember = self.theta_last is None or (abs(self.theta_last-theta_list)>1e-12).sum() > 1
+
+        mats = []
+        theta_last = self.theta_last
+        if remember:
+            self.theta_last = theta_list.copy()
+
+        qureg_ = qureg
+        for iblock, block in enumerate(self):
+            # generate or use a block matrix
+            num_param = block.num_param
+            theta_i, theta_list = np.split(theta_list, [num_param])
+            if theta_last is not None:
+                theta_o, theta_last = np.split(theta_last, [num_param])
+            if self.memo is not None and (num_param==0 or np.abs(theta_i-theta_o).max()<1e-12):
+                # use data cached in memory
+                mat = self.memo[iblock]
+            else:
+                if self.memo is not None and not remember:
+                    # update the changed gate, but not touching memory.
+                    mat = _rot_tocsr_update1(block, self.memo[iblock], theta_o, theta_i)
+                else:
+                    # regenerate one
+                    mat = block.tocsr(theta_i)
+            for mat_i in mat:
+                qureg_ = mat_i.dot(qureg_)
+            mats.append(mat)
+
+        if remember:
+            # cache data
+            self.memo = mats
+        # update register
+        qureg[...] = qureg_
+        np.testing.assert_(len(theta_list)==0)
+        
+    @property
+    def num_bit(self):
+        return self[0].num_bit
+
+    @property
+    def num_param(self):
+        return sum([b.num_param for b in self])
+
+def _rot_tocsr_update1(layer, old, theta_old, theta_new):
+    '''
+    rotation layer csr_matrices update method.
+    
+    Args:
+        layer (ArbitraryRotation): rotation layer.
+        old (csr_matrix): old matrices.
+        theta_old (1darray): old parameters.
+        theta_new (1darray): new parameters.
+
+    Returns:
+        list of csr_matrix: new rotation matrices after the theta changed.
+    '''
+    idiff_param = np.where(abs(theta_old-theta_new)>1e-12)[0].item()
+    idiff = np.where(layer.mask)[0][idiff_param]
+
+    # get rotation parameters
+    isite = idiff//3
+    theta_list_ = np.zeros(3*layer.num_bit)
+    theta_list_[layer.mask] = theta_new
+    
+    new = old[:]
+    new[isite] = compiler(rot(*theta_list_[isite*3:isite*3+3]), isite, layer.num_bit)
+    return new
