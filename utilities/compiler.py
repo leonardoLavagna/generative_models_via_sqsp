ff3626f6d7af4e6ee5ed2fbb8ef20073dce5327a
Leonardo Lavagna <llavagna@proton.me>
2025-02-15 14:15:57 +0100

    Update compiler.py

--- utilities/compiler.py
+++ utilities/compiler.py
@@ -78,29 +78,6 @@ def _wrap_identity(data_list, num_bit_list):
         res = sps.kron(res, sps.eye(2**nbit, dtype='complex128'))  # Apply identity operators to the other segments
     return res
 
-def _rot_tocsr_update1(layer, old, theta_old, theta_new):
-    '''
-    rotation layer csr_matrices update method.
-
-    Args:
-        layer (ArbitraryRotation): rotation layer.
-        old (csr_matrix): old matrices.
-        theta_old (1darray): old parameters.
-        theta_new (1darray): new parameters.
-
-    Returns:
-        list of csr_matrix: new rotation matrices after the theta changed.
-    '''
-    idiff_param = np.where(abs(theta_old-theta_new)>1e-12)[0].item()
-    idiff = np.where(layer.mask)[0][idiff_param]
-    # get rotation parameters
-    isite = idiff//3
-    theta_list_ = np.zeros(3*layer.num_bit)
-    theta_list_[layer.mask] = theta_new
-    new = old[:]
-    new[isite] = compiler(rot(*theta_list_[isite*3:isite*3+3]), isite, layer.num_bit)
-    return new
-
 
 def initial_wf(num_bit):
     '''
