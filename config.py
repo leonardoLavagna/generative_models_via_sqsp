7c4908a214d960a00c868bb2339662e0e63efb29
Leonardo Lavagna <llavagna@proton.me>
2025-02-19 14:31:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 from qiskit_aer import Aer
+from scipy.stats import beta
 
+# Quantum backend simulator
 backend = Aer.get_backend("aer_simulator")
-m = 4
 shots = 1024
+
+# Number of qubits
+m = 4
+
+# Optimizer 
 optimizer_type = "COBYLA"
 max_iterations = 20
 runs = 10
+
+# Target distribution
 max_trainable_params = 2**m-1
+beta_1 = 2
+beta_2 = 2
+a = 0
+b = 1     
