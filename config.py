8af97d92ed18ecae949e791a4ad2ebdbf687b788
Leonardo Lavagna <llavagna@proton.me>
2025-02-19 14:32:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ from scipy.stats import beta
 backend = Aer.get_backend("aer_simulator")
 shots = 1024
 
-# Number of qubits
+# Number of qubits and trainable parameters
 m = 4
+max_trainable_params = 2**m-1
 
 # Optimizer 
 optimizer_type = "COBYLA"
@@ -14,7 +15,6 @@ max_iterations = 20
 runs = 10
 
 # Target distribution
-max_trainable_params = 2**m-1
 alpha_ = 2
 beta_ = 2
 a = 0
